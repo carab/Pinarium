d545f2465d21175d2b4baebcc7549595fd085f1a
Camille RABATEL <camille.rabatel+github@gmail.com>
2016-01-07 12:00:51 +0100

    make routing working with Auth service

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -64,108 +64,133 @@
   }
 
   /** @ngInject */
-  function router($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/auth');
+  function router($stateProvider, $urlRouterProvider, $transitionsProvider) {
+    $urlRouterProvider.otherwise('/app');
 
-  $stateProvider
-    .state('auth', {
-      url: '/auth',
-      template: '<auth layout layout-fill></auth>',
-      onEnter: function($state, Auth) {
-        Auth.$requireAuth().then(function() {
-          $state.go('app.bottles');
-        });
+    $transitionsProvider.onStart({
+      to: function(state) {
+        return state.requiresAuth;
       }
-    })
+    }, requiresAuth);
 
-    .state('app', {
-      url: '/app',
-      abstract: true,
-      template: '<main layout layout-fill></main>',
-      resolve: {
-        /** ngInject */
-        auth: function(Auth) {
-          return Auth.$requireAuth();
+    $transitionsProvider.onStart({
+      to: function(state) {
+        return state.requiresUnauth;
+      }
+    }, requiresUnauth);
+
+    /** @ngInject */
+    function requiresAuth($state, Auth) {
+      return Auth.$requireAuth().catch(function() { return $state.go('auth'); });
+    }
+
+    /** @ngInject */
+    function requiresUnauth($state, $q, Auth) {
+      var deferred = $q.defer();
+
+      Auth.$requireAuth()
+        .then(function (data) { deferred.reject(data) })
+        .catch(function (data) { deferred.resolve(data) });
+
+      return deferred.promise.catch(function() { return $state.go('app.bottles'); });
+    }
+
+    $stateProvider
+      .state('auth', {
+        url: '/auth',
+        requiresUnauth: true,
+        template: '<auth layout layout-fill></auth>'
+      })
+
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        requiresAuth: true,
+        template: '<main layout layout-fill></main>'
+      })
+
+      .state('app.bottles', {
+        url: '',
+        requiresAuth: true,
+        template: '<bottle-list></bottle-list>'
+      })
+
+      .state('app.bottles.add', {
+        url: '/add',
+        requiresAuth: true,
+        params: {
+          event: null
+        },
+        /** @ngInject */
+        onEnter: function($state, $stateParams, BottleFormDialog) {
+          BottleFormDialog.show($stateParams.event).finally(function() {
+            $state.go('^');
+          });
+        },
+        /** @ngInject */
+        onExit: function(BottleFormDialog) {
+          BottleFormDialog.close();
         }
-      }
-    })
+      })
 
-    .state('app.bottles', {
-      url: '',
-      template: '<bottle-list></bottle-list>'
-    })
+      .state('app.bottles.edit', {
+        url: '/edit/:id',
+        requiresAuth: true,
+        params: {
+          event: null
+        },
+        /** @ngInject */
+        onEnter: function($state, $stateParams, BottleFormDialog) {
+          BottleFormDialog.show($stateParams.event, $stateParams.id).finally(function() {
+            $state.go('^');
+          });
+        },
+        /** @ngInject */
+        onExit: function(BottleFormDialog) {
+          BottleFormDialog.close();
+        }
+      })
 
-    .state('app.bottles.add', {
-      url: '/add',
-      params: {
-        event: null
-      },
-      /** ngInject */
-      onEnter: function($state, $stateParams, BottleFormDialog) {
-        BottleFormDialog.show($stateParams.event).finally(function() {
-          $state.go('^');
-        });
-      },
-      /** ngInject */
-      onExit: function(BottleFormDialog) {
-        BottleFormDialog.close();
-      }
-    })
+      .state('app.caves', {
+        url: '/caves',
+        requiresAuth: true,
+        template: '<cave-list></cave-list>'
+      })
 
-    .state('app.bottles.edit', {
-      url: '/edit/:id',
-      params: {
-        event: null
-      },
-      /** ngInject */
-      onEnter: function($state, $stateParams, BottleFormDialog) {
-        BottleFormDialog.show($stateParams.event, $stateParams.id).finally(function() {
-          $state.go('^');
-        });
-      },
-      /** ngInject */
-      onExit: function(BottleFormDialog) {
-        BottleFormDialog.close();
-      }
-    })
+      .state('app.caves.add', {
+        url: '/add',
+        requiresAuth: true,
+        params: {
+          event: null
+        },
+        /** @ngInject */
+        onEnter: function($state, $stateParams, CaveFormDialog) {
+          CaveFormDialog.show($stateParams.event).finally(function() {
+            $state.go('^');
+          });
+        },
+        /** @ngInject */
+        onExit: function(CaveFormDialog) {
+          CaveFormDialog.close();
+        }
+      })
 
-    .state('app.caves', {
-      url: '/caves',
-      template: '<cave-list></cave-list>'
-    })
-
-    .state('app.caves.add', {
-      url: '/add',
-      params: {
-        event: null
-      },
-      /** ngInject */
-      onEnter: function($state, $stateParams, CaveFormDialog) {
-        CaveFormDialog.show($stateParams.event).finally(function() {
-          $state.go('^');
-        });
-      },
-      /** ngInject */
-      onExit: function(CaveFormDialog) {
-        CaveFormDialog.close();
-      }
-    })
-
-    .state('app.caves.edit', {
-      url: '/edit/:id',
-      params: {
-        event: null
-      },
-      /** ngInject */
-      onEnter: function($state, $stateParams, CaveFormDialog) {
-        CaveFormDialog.show($stateParams.event, $stateParams.id).finally(function() {
-          $state.go('^');
-        });
-      },
-      /** ngInject */
-      onExit: function(CaveFormDialog) {
-        CaveFormDialog.close();
-      }
-    });
+      .state('app.caves.edit', {
+        url: '/edit/:id',
+        requiresAuth: true,
+        params: {
+          event: null
+        },
+        /** @ngInject */
+        onEnter: function($state, $stateParams, CaveFormDialog) {
+          CaveFormDialog.show($stateParams.event, $stateParams.id).finally(function() {
+            $state.go('^');
+          });
+        },
+        /** @ngInject */
+        onExit: function(CaveFormDialog) {
+          CaveFormDialog.close();
+        }
+      });
   }
 })();
